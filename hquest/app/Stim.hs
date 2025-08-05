dac8f5227bee0380dbad52f3b5886a00aa8f745c
le.niu <le.niu@hotmail.com>
2025-08-05 23:14:17 +0800

    start stim support

---
+++ hquest/app/Stim.hs
@@ -0,0 +1,15 @@
+module Stim where 
+import Expr
+import qualified StimParser.Expr as STE
+-- import StimParser.Parse
+-- import StimParser.ParseUtils
+
+-- type StimEnvState = State (Env, )
+
+-- class Stim2Expr a where 
+--   type QuestExpr a 
+--   stim2expr :: a -> EnvState (QuestExpr a)
+
+-- instance Stim2Expr 
+-- stim2expr :: STE.Stim -> EnvState Circuit
+-- stim2expr STE.StimG  
